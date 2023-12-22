bc74b432ac8e3c335b0813ab82bbdc21ad3058d7
Ricky Barnett <10067391+ricky-barnett@users.noreply.github.com>
2023-12-22 21:08:40 +0000

    Update simulation.rs

--- src/simulation.rs
+++ src/simulation.rs
@@ -149,8 +149,8 @@ impl<'de> Deserialize<'de> for PermissiveUint {
 fn chain_id_to_fork_url(chain_id: u64) -> Result<String, Rejection> {
     match chain_id {
         // ethereum
-        1 => Ok("https://dimensional-damp-diagram.quiknode.pro/851961e1801845d45694beddd6eeeb72cc01d3d3/".to_string()),
-        5 => Ok("http://172.17.0.2:8545".to_string()),
+        1 => Ok("http://172.18.0.2:8545".to_string()),
+        5 => Ok("http://172.18.0.2:8545".to_string()),
         11155111 => Ok("https://eth-sepolia.g.alchemy.com/v2/demo".to_string()),
         // polygon
         137 => Ok("https://polygon-mainnet.g.alchemy.com/v2/demo".to_string()),
